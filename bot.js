889d8e75bd97626d4f02987785eed9ffae01cac7
BooDyyi <50505158+BooDyyi@users.noreply.github.com>
2019-09-20 12:41:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['340545352971059201'];
 const db = require('quick.db');
 const premium = ['470896018603376640']
 const client = new Discord.Client();   
